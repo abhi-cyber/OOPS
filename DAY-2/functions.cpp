26cbec34325e25ed2b64c9b8d82027f51fbba4b6
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-18 20:30:16 +0530

    Function Syntex

---
+++ DAY-2/functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+    sytanx of a function <> -> Mandatory [] -> Not Mandatory
+
+    // Declaration
+
+    <return type> <function name>([Parameters if any] < with Data Typeˀ>){
+        set of statements
+    }
+
+    // Calling
+
+    <function name>([Parameters if any]);
+
+    // When a function return something it need to be asigned to a variable
+*/
